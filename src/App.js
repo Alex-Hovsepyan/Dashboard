b2e04786e860dae7175daa66b00b833798634d9b
Nona Karapetyan <nonkarapetyan@Nonas-MacBook-Pro.local>
2022-08-11 16:51:48 +0400

    Left part background fixed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import CustomButton from "./Custom/CustomButton/CustomButton";
 function App() {
   return (
     <div>
-        {/* <LeftPartBackground src = {'sinInImg'} /> */}
-        <CustomInput label={"Name"} width={300} placeholder={"Your password..."} />
+        <LeftPartBackground src = {'sinInImg'} />
+        {/* <CustomInput label={"Name"} width={300} placeholder={"Your password..."} /> */}
         {/* <CustomButton value={"sign in"} /> */}
     </div>
   );
--- src/LeftPartBackground/LeftPartBackground.js
+++ src/LeftPartBackground/LeftPartBackground.js
@@ -3,12 +3,12 @@ import sinInImg from "../assets/images/singInImg.png";
 import sinUpImg from "../assets/images/signUpImg.png";
 import "./LeftPartBackground.css"
 
-function LeftPartBackground({src}) {
+function LeftPartBackground({ src }) {
     let change;
-    src == 'sinInImg'? change = sinInImg : change = sinUpImg
+    src === 'sinInImg'? change = sinInImg : change = sinUpImg
     return (
         <div className="container">
-            <img src={change} alt={"Background image"} />
+            <img src={change} alt={"Background"} />
             <div className="text">
                 <p>inspired by the future:</p>
                 <h6>the vision ui dashboard</h6>
