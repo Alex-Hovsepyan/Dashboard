9288d4543fda9ececc37cfa33ee240f2b22d9509
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2022-08-15 12:34:01 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import SignUp from "../src/SignUp/SignUp";
+// import SignUp from "../src/SignUp/SignUp";
 import React from "react";
 // import LeftPartBackground from "./LeftPartBackground/LeftPartBackground";
 // import CustomInput from "./Custom/CustomInput/CustomInput"
 // import CustomButton from "./Custom/CustomButton/CustomButton";
-import SignIn from "./SignIn/SignIn";
-
+// import SignIn from "./SignIn/SignIn";
+import CustomTable from "./Custom/CustomTable/CustomTable";
 function App(){
   // const location = document.location.pathname;
   // console.log(location)
   return (
     <div>
+      {/* <CustomTable/> */}
       {/* <SignIn /> */}
       {/* {location === "/signIn" ? <SignIn /> : <SignUp />} */}
       {/* <SignIn/> */}
