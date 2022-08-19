afceab594d594614a80dda91629a68bffde92940
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2022-08-19 15:40:06 +0400

    finish

--- src/Tables/Tables.js
+++ src/Tables/Tables.js
@@ -2,13 +2,20 @@ import CustomTable from "../Custom/CustomTable/CustomTable";
 import GeneralHeader from "../GeneralHeader/GeneralHeader";
 import Sidebar from "../Sidebar/Sidebar";
 import { dataAuthors } from "../fakeData/constants";
+import s from  "../Custom/CustomTable/CustomTable.module.scss";
+
+
 
 function Tables() {
     return(<div style={{display: 'flex'}}>
-            <Sidebar />
+        <div className={s.closeSidebar}>
+        <Sidebar />
+
+        </div>
+            
             <div style={{width: "calc(100% - 280px)", marginLeft: 15}} >
                 <GeneralHeader locationName={"Tables"} />
-                <div style={{width: "100%", marginLeft: 250}}>
+                <div style={{marginLeft : 250,width: "100%"}}>
                     <CustomTable data={dataAuthors} />
                 </div>
             </div>
