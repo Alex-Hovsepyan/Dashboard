418daaf824795065acca2515e5a1fbb833f486ab
Nona Karapetyan <nonkarapetyan@Nonas-MacBook-Pro.local>
2022-08-19 16:25:54 +0400

    Car and profile informations

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,7 +1,20 @@
+import GeneralHeader from "../GeneralHeader/GeneralHeader";
+import Sidebar from "../Sidebar/Sidebar";
+import CarInformation from "./CarInformation/CarInformation";
+import s from "./Profile.module.scss";
+import ProfileInformation from "./ProfileInformation/ProfileInformtion";
+
 function Profile() {
     return (
-        <div>
-            Profile
+        <div className={s.profileContainer}>
+            <Sidebar />
+            <div className={s.mainContainer}>
+                <div className={s.header}>
+                    <GeneralHeader locationName={"Profile"}/>
+                </div>
+                <div className={s.mainPart}>
+                </div>
+            </div>
         </div>
     )
 }
